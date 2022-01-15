276835b7fb6029f525da076dd192890920ec7309
Jenika Laira Macaso <jenika@bbtdigital.com>
2022-01-15 10:20:03 +0800

    ** added scripts

--- public/electron.js
+++ public/electron.js
@@ -1,47 +1,83 @@
+const { app, BrowserWindow } = require('electron'); // electron
+const isDev = require('electron-is-dev'); // To check if electron is in development mode
 const path = require('path');
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+let mainWindow;
 
-function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+// Initializing the Electron Window
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 600, // width of window
+    height: 600, // height of window
     webPreferences: {
-      nodeIntegration: true,
+      // The preload file where we will perform our app communication
+      preload: isDev 
+        ? path.join(app.getAppPath(), './public/preload.js') // Loading it from the public folder for dev
+        : path.join(app.getAppPath(), './build/preload.js'), // Loading it from the build folder for production
+      worldSafeExecuteJavaScript: true, // If you're using Electron 12+, this should be enabled by default and does not need to be added here.
+      contextIsolation: true, // Isolating context so our app is not exposed to random javascript executions making it safer.
     },
   });
 
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
+	// Loading a webpage inside the electron window we just created
+  mainWindow.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      ? 'http://localhost:3000' // Loading localhost if dev mode
+      : `file://${path.join(__dirname, '../build/index.html')}` // Loading build file if in production
   );
-  // Open the DevTools.
+
+	// Setting Window Icon - Asset file needs to be in the public/images folder.
+  mainWindow.setIcon(path.join(__dirname, 'images/appicon.ico'));
+
+	// In development mode, if the window has loaded, then load the dev tools.
   if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
+    mainWindow.webContents.on('did-frame-finish-load', () => {
+      mainWindow.webContents.openDevTools({ mode: 'detach' });
+    });
   }
-}
+};
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+// ((OPTIONAL)) Setting the location for the userdata folder created by an Electron app. It default to the AppData folder if you don't set it.
+app.setPath(
+  'userData',
+  isDev
+    ? path.join(app.getAppPath(), 'userdata/') // In development it creates the userdata folder where package.json is
+    : path.join(process.resourcesPath, 'userdata/') // In production it creates userdata folder in the resources folder
+);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// When the app is ready to load
+app.whenReady().then(async () => {
+  await createWindow(); // Create the mainWindow
+
+  // If you want to add React Dev Tools
+  if (isDev) {
+    await session.defaultSession
+      .loadExtension(
+        path.join(__dirname, `../userdata/extensions/react-dev-tools`) // This folder should have the chrome extension for React Dev Tools. Get it online or from your Chrome extensions folder.
+      )
+      .then((name) => console.log('Dev Tools Loaded'))
+      .catch((err) => console.log(err));
+  }
+});
+
+// Exiting the app
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// Activating the app
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
+  if (mainWindow.getAllWindows().length === 0) {
     createWindow();
+  }
+});
+
+// Logging any exceptions
+process.on('uncaughtException', (error) => {
+  console.log(`Exception: ${error}`);
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
