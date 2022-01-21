b50663b23eef7eca91bc0e5efe3c58cbdb995e1f
Alexa Dayandayan <alexa@bbtdigital.com>
2022-01-21 14:08:14 +0800

    eletron nextjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    compress: true,
-    poweredByHeader: false,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    }
-};
--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../assets/scss/_main.scss";
-
-const App = ({ Component, pageProps }) => {
-    return <Component className="App" {...pageProps} />
-}
-
-export default App
